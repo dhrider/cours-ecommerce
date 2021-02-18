a36d9494903316c29bedbf16b4770a56a6b4708c
Hetphil <p_bordmann@orange.fr>
2021-02-18 15:45:23 +0100

    - Doctrine - Entity

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use Bezhanov\Faker\Provider\Commerce;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Liior\Faker\Prices;
+use Mmo\Faker\PicsumProvider;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AppFixtures extends Fixture
+{
+    protected $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new Prices($faker));
+        $faker->addProvider(new Commerce($faker));
+        $faker->addProvider(new PicsumProvider($faker));
+
+        for($c = 0; $c < 3; $c++) {
+            $category = new Category();
+            $category->setName($faker->department);
+            $category->setSlug(strtolower($this->slugger->slug($category->getName())));
+            $manager->persist($category);
+
+            for($i = 0; $i < mt_rand(15, 20); $i++) {
+                $product = new Product();
+                $product
+                    ->setName($faker->productName)
+                    ->setPrice($faker->price(4000, 20000))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph())
+                    ->setMainPicture($faker->picsumUrl(400, 400))
+                ;
+                $manager->persist($product);
+            }
+        }
+
+        $manager->flush();
+    }
+}
